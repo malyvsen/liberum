79b345c1edb0de32411e7a4488ee3d778dc33993
Franciszek Madej <f.madej@protonmail.com>
2019-08-16 17:33:41 +0200

    feat(frontend): add publicKey formatting

--- screens/YouScreen.js
+++ screens/YouScreen.js
@@ -6,12 +6,24 @@ import { GraphContext } from "../App";
 
 export default function YouScreen() {
   const graph = useContext(GraphContext);
-  const { name, publicKey } = graph.currentAccount;
+  const { name, publicKey } = graph ? graph.currentAccount : null;
+
+  // i think that's pretty cool
+  const formattedPublicKey = publicKey
+    .split("")
+    .map((letter, index) => {
+      return index !== 0 && (index + 1) % 4 === 0 ? letter + "-" : letter;
+    })
+    .join("")
+    .split("")
+    .slice(0, -1)
+    .join("");
+
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 24 }}>
       <View style={{ padding: 24 }}>
         <BigMonoText>Hello @{name}!</BigMonoText>
-        <MonoText>{publicKey}</MonoText>
+        <MonoText>{formattedPublicKey}</MonoText>
       </View>
       <View>
         <View
