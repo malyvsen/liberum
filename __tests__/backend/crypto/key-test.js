33df27ae0e45ef59b1c310bb4c0ebf465bcb2b97
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-28 15:58:08 +0200

    test(crypto): use 512-bit keys for speed

--- __tests__/backend/crypto/key-test.js
+++ __tests__/backend/crypto/key-test.js
@@ -5,8 +5,8 @@ test("same passwords give same public keys", () => {
   const key = new Key();
   const sameKey = new Key();
   const password = "Here we go!";
-  return key.generate(password, 1024).then(() => {
-    return sameKey.generate(password, 1024).then(() => {
+  return key.generate(password, 512).then(() => {
+    return sameKey.generate(password, 512).then(() => {
       expect(sameKey.publicKey).toBe(key.publicKey);
     });
   });
@@ -15,8 +15,8 @@ test("same passwords give same public keys", () => {
 test("different passwords give different public keys", () => {
   const key = new Key();
   const differentKey = new Key();
-  return key.generate("Here we go!", 1024).then(() => {
-    return differentKey.generate("No we don't", 1024).then(() => {
+  return key.generate("Here we go!", 512).then(() => {
+    return differentKey.generate("No we don't", 512).then(() => {
       expect(differentKey.publicKey).not.toBe(key.publicKey);
     });
   });
@@ -27,7 +27,7 @@ test("signatures can be verified using public key only", () => {
   const verifyingKey = new Key();
   const password = "Here we go!";
   const plaintext = "Clear, legible plaintext";
-  return signingKey.generate(password, 1024).then(() => {
+  return signingKey.generate(password, 512).then(() => {
     verifyingKey.publicKey = signingKey.publicKey;
     return signingKey.sign(plaintext).then(signature => {
       return verifyingKey.verify(plaintext, signature).then(status => {
@@ -42,7 +42,7 @@ test("forged signature is identified as such", () => {
   const verifyingKey = new Key();
   const password = "Moo";
   const plaintext = "Text someone wants to mess with";
-  return signingKey.generate(password, 1024).then(() => {
+  return signingKey.generate(password, 512).then(() => {
     verifyingKey.publicKey = signingKey.publicKey;
     return signingKey.sign(plaintext).then(signature => {
       const forgedSignature = signature
@@ -59,10 +59,10 @@ test("forged signature is identified as such", () => {
 test("the same passwords yields the same key across runs", () => {
   const key = new Key();
   const password = "Here we go!";
-  return key.generate(password).then(() => {
+  return key.generate(password, 512).then(() => {
     console.log(key.publicKey);
     expect(key.publicKey).toBe(
-      "hGsN2fJJOCqbM8eZsrJzBbLGYMhUIWMe5gQcpuSRaMr/3mJr/6ZfmpYNGdX2iwS/b295D0lE0y5Eqs24AK3jmjb/3mqyhrP64r6YN/umduFRTutqL+xZHsMP5TTjgv8CzFwzlqGO267ZJ4XT9kJ5oeIHUaVWdz1jHQ3PPZaTRgGgGWhILWoGPkLJ3CKukIPaHP4jJxYnOTx6EB01Ain24BMcGHqV21Ptvwcl3kmoQsOQ9iUZGh1RKkJe4RQm1JSPBvn8S6wz0oWELAVxsHBs13y0ZBHZZDcDwo46CW0cfI4+/sXI4SOQRkpwpoVzYB4u4FMvTQiPQ+UxQ7O9GG5X9Q=="
+      "bqpr4Z8s6ONEhX1xU095RiWCj9YcNpXd4wMgaMC7skstYkKaLC68b77PLAPtdHCdzESVWmi4CxwvGxQpvE849Q=="
     );
   });
 });
