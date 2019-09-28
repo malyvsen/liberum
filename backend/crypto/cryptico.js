3e82b05cd1c16388d075cb916b7e6af0d2d7e0d3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-09-28 13:05:29 +0200

    refactor(crypto): restructure cryptico as module

--- backend/crypto/cryptico.js
+++ backend/crypto/cryptico.js
@@ -1,5 +1,3 @@
-// Adapted for use in last-id from https://github.com/wwwtyro/cryptico
-
 // Copyright (c) 2005  Tom Wu
 // All Rights Reserved.
 // See "LICENSE" for details.
@@ -3252,222 +3250,215 @@ RSAKey.prototype.signStringWithSHA256 = _rsasign_signStringWithSHA256;
 RSAKey.prototype.verifyString = _rsasign_verifyString;
 RSAKey.prototype.verifyHexSignatureForMessage = _rsasign_verifyHexSignatureForMessage;
 
-var cryptico = (function() {
-  var my = {};
+// Adapted for use in last-id from https://github.com/wwwtyro/cryptico
+// Cryptico could not be used as is because of security and dependency issues
 
-  var base64Chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+export default {
+  sign: function(plaintext, key) {
+    return b16to64(key.signString(plaintext, "sha256"));
+  },
 
-  my.b256to64 = function(t) {
-    var a, c, n;
-    var r = "",
-      l = 0,
-      s = 0;
-    var tl = t.length;
-    for (n = 0; n < tl; n++) {
-      c = t.charCodeAt(n);
-      if (s == 0) {
-        r += base64Chars.charAt((c >> 2) & 63);
-        a = (c & 3) << 4;
-      } else if (s == 1) {
-        r += base64Chars.charAt(a | ((c >> 4) & 15));
-        a = (c & 15) << 2;
-      } else if (s == 2) {
-        r += base64Chars.charAt(a | ((c >> 6) & 3));
-        l += 1;
-        r += base64Chars.charAt(c & 63);
-      }
-      l += 1;
-      s += 1;
-      if (s == 3) s = 0;
-    }
-    if (s > 0) {
-      r += base64Chars.charAt(a);
-      l += 1;
-      r += "=";
-      l += 1;
-    }
-    if (s == 1) {
-      r += "=";
-    }
-    return r;
-  };
-
-  my.b64to256 = function(t) {
-    var c, n;
-    var r = "",
-      s = 0,
-      a = 0;
-    var tl = t.length;
-    for (n = 0; n < tl; n++) {
-      c = base64Chars.indexOf(t.charAt(n));
-      if (c >= 0) {
-        if (s) r += String.fromCharCode(a | ((c >> (6 - s)) & 255));
-        s = (s + 2) & 7;
-        a = (c << s) & 255;
-      }
-    }
-    return r;
-  };
-
-  my.b16to64 = function(h) {
-    var i;
-    var c;
-    var ret = "";
-    if (h.length % 2 == 1) {
-      h = "0" + h;
-    }
-    for (i = 0; i + 3 <= h.length; i += 3) {
-      c = parseInt(h.substring(i, i + 3), 16);
-      ret += base64Chars.charAt(c >> 6) + base64Chars.charAt(c & 63);
-    }
-    if (i + 1 == h.length) {
-      c = parseInt(h.substring(i, i + 1), 16);
-      ret += base64Chars.charAt(c << 2);
-    } else if (i + 2 == h.length) {
-      c = parseInt(h.substring(i, i + 2), 16);
-      ret += base64Chars.charAt(c >> 2) + base64Chars.charAt((c & 3) << 4);
-    }
-    while ((ret.length & 3) > 0) ret += "=";
-    return ret;
-  };
-
-  my.b64to16 = function(s) {
-    var ret = "";
-    var i;
-    var k = 0;
-    var slop;
-    for (i = 0; i < s.length; ++i) {
-      if (s.charAt(i) == "=") break;
-      v = base64Chars.indexOf(s.charAt(i));
-      if (v < 0) continue;
-      if (k == 0) {
-        ret += int2char(v >> 2);
-        slop = v & 3;
-        k = 1;
-      } else if (k == 1) {
-        ret += int2char((slop << 2) | (v >> 4));
-        slop = v & 0xf;
-        k = 2;
-      } else if (k == 2) {
-        ret += int2char(slop);
-        ret += int2char(v >> 2);
-        slop = v & 3;
-        k = 3;
-      } else {
-        ret += int2char((slop << 2) | (v >> 4));
-        ret += int2char(v & 0xf);
-        k = 0;
-      }
-    }
-    if (k == 1) ret += int2char(slop << 2);
-    return ret;
-  };
-
-  // Converts a string to a byte array.
-  my.string2bytes = function(string) {
-    var bytes = new Array();
-    for (var i = 0; i < string.length; i++) {
-      bytes.push(string.charCodeAt(i));
-    }
-    return bytes;
-  };
-
-  // Converts a byte array to a string.
-  my.bytes2string = function(bytes) {
-    var string = "";
-    for (var i = 0; i < bytes.length; i++) {
-      string += String.fromCharCode(bytes[i]);
-    }
-    return string;
-  };
-
-  // Returns a XOR b, where a and b are 16-byte byte arrays.
-  my.blockXOR = function(a, b) {
-    var xor = new Array(16);
-    for (var i = 0; i < 16; i++) {
-      xor[i] = a[i] ^ b[i];
-    }
-    return xor;
-  };
-
-  // Returns a 16-byte initialization vector.
-  my.blockIV = function() {
-    var r = new SecureRandom();
-    var IV = new Array(16);
-    r.nextBytes(IV);
-    return IV;
-  };
-
-  // Returns a copy of bytes with zeros appended to the end
-  // so that the (length of bytes) % 16 == 0.
-  my.pad16 = function(bytes) {
-    var newBytes = bytes.slice(0);
-    var padding = (16 - (bytes.length % 16)) % 16;
-    for (i = bytes.length; i < bytes.length + padding; i++) {
-      newBytes.push(0);
-    }
-    return newBytes;
-  };
-
-  // Removes trailing zeros from a byte array.
-  my.depad = function(bytes) {
-    var newBytes = bytes.slice(0);
-    while (newBytes[newBytes.length - 1] == 0) {
-      newBytes = newBytes.slice(0, newBytes.length - 1);
-    }
-    return newBytes;
-  };
-
-  // Wraps a string to 60 characters.
-  my.wrap60 = function(string) {
-    var outstr = "";
-    for (var i = 0; i < string.length; i++) {
-      if (i % 60 == 0 && i != 0) outstr += "\n";
-      outstr += string[i];
-    }
-    return outstr;
-  };
-
-  // Generates an RSA key from a passphrase.
-  my.generateRSAKey = function(passphrase, bitlength) {
-    Math.seedrandom(sha256.hex(passphrase));
-    var rsa = new RSAKey();
-    rsa.generate(bitlength, "03");
-    return rsa;
-  };
-
-  // Returns the ascii-armored version of the public key.
-  my.publicKeyString = function(rsakey) {
-    var pubkey = my.b16to64(rsakey.n.toString(16));
-    return pubkey;
-  };
-
-  // Returns an MD5 sum of a publicKeyString for easier identification.
-  my.publicKeyID = function(publicKeyString) {
-    return MD5(publicKeyString);
-  };
-
-  my.publicKeyFromString = function(string) {
-    var N = my.b64to16(string.split("|")[0]);
-    var E = "03";
-    var rsa = new RSAKey();
-    rsa.setPublic(N, E);
-    return rsa;
-  };
-
-  my.sign = function(plaintext, key) {
-    return cryptico.b16to64(key.signString(plaintext, "sha256"));
-  };
-
-  my.verify = function(plaintext, signature, publicKeyString) {
-    const publicKey = my.publicKeyFromString(publicKeyString);
-    const signature16 = my.b64to16(signature);
+  verify: function(plaintext, signature, publicKeyString) {
+    const publicKey = publicKeyFromString(publicKeyString);
+    const signature16 = b64to16(signature);
     return publicKey.verifyString(plaintext, signature16);
-  };
+  },
 
-  return my;
-})();
+  generateRSAKey: function(password, bitLength) {
+    Math.seedrandom(sha256.hex(password));
+    var rsa = new RSAKey();
+    rsa.generate(bitLength, "03");
+    return rsa;
+  },
 
-export default cryptico;
-// module.exports = cryptico;
+  publicKeyString: function(rsakey) {
+    return b16to64(rsakey.n.toString(16));
+  }
+};
+
+const base64Chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+function b256to64(t) {
+  var a, c, n;
+  var r = "",
+    l = 0,
+    s = 0;
+  var tl = t.length;
+  for (n = 0; n < tl; n++) {
+    c = t.charCodeAt(n);
+    if (s == 0) {
+      r += base64Chars.charAt((c >> 2) & 63);
+      a = (c & 3) << 4;
+    } else if (s == 1) {
+      r += base64Chars.charAt(a | ((c >> 4) & 15));
+      a = (c & 15) << 2;
+    } else if (s == 2) {
+      r += base64Chars.charAt(a | ((c >> 6) & 3));
+      l += 1;
+      r += base64Chars.charAt(c & 63);
+    }
+    l += 1;
+    s += 1;
+    if (s == 3) s = 0;
+  }
+  if (s > 0) {
+    r += base64Chars.charAt(a);
+    l += 1;
+    r += "=";
+    l += 1;
+  }
+  if (s == 1) {
+    r += "=";
+  }
+  return r;
+}
+
+function b64to256(t) {
+  var c, n;
+  var r = "",
+    s = 0,
+    a = 0;
+  var tl = t.length;
+  for (n = 0; n < tl; n++) {
+    c = base64Chars.indexOf(t.charAt(n));
+    if (c >= 0) {
+      if (s) r += String.fromCharCode(a | ((c >> (6 - s)) & 255));
+      s = (s + 2) & 7;
+      a = (c << s) & 255;
+    }
+  }
+  return r;
+}
+
+function b16to64(h) {
+  var i;
+  var c;
+  var ret = "";
+  if (h.length % 2 == 1) {
+    h = "0" + h;
+  }
+  for (i = 0; i + 3 <= h.length; i += 3) {
+    c = parseInt(h.substring(i, i + 3), 16);
+    ret += base64Chars.charAt(c >> 6) + base64Chars.charAt(c & 63);
+  }
+  if (i + 1 == h.length) {
+    c = parseInt(h.substring(i, i + 1), 16);
+    ret += base64Chars.charAt(c << 2);
+  } else if (i + 2 == h.length) {
+    c = parseInt(h.substring(i, i + 2), 16);
+    ret += base64Chars.charAt(c >> 2) + base64Chars.charAt((c & 3) << 4);
+  }
+  while ((ret.length & 3) > 0) ret += "=";
+  return ret;
+}
+
+function b64to16(s) {
+  var ret = "";
+  var i;
+  var k = 0;
+  var slop;
+  for (i = 0; i < s.length; ++i) {
+    if (s.charAt(i) == "=") break;
+    v = base64Chars.indexOf(s.charAt(i));
+    if (v < 0) continue;
+    if (k == 0) {
+      ret += int2char(v >> 2);
+      slop = v & 3;
+      k = 1;
+    } else if (k == 1) {
+      ret += int2char((slop << 2) | (v >> 4));
+      slop = v & 0xf;
+      k = 2;
+    } else if (k == 2) {
+      ret += int2char(slop);
+      ret += int2char(v >> 2);
+      slop = v & 3;
+      k = 3;
+    } else {
+      ret += int2char((slop << 2) | (v >> 4));
+      ret += int2char(v & 0xf);
+      k = 0;
+    }
+  }
+  if (k == 1) ret += int2char(slop << 2);
+  return ret;
+}
+
+// Converts a string to a byte array.
+function string2bytes(string) {
+  var bytes = new Array();
+  for (var i = 0; i < string.length; i++) {
+    bytes.push(string.charCodeAt(i));
+  }
+  return bytes;
+}
+
+// Converts a byte array to a string.
+function bytes2string(bytes) {
+  var string = "";
+  for (var i = 0; i < bytes.length; i++) {
+    string += String.fromCharCode(bytes[i]);
+  }
+  return string;
+}
+
+// Returns a XOR b, where a and b are 16-byte byte arrays.
+function blockXOR(a, b) {
+  var xor = new Array(16);
+  for (var i = 0; i < 16; i++) {
+    xor[i] = a[i] ^ b[i];
+  }
+  return xor;
+}
+
+// Returns a 16-byte initialization vector.
+function blockIV() {
+  var r = new SecureRandom();
+  var IV = new Array(16);
+  r.nextBytes(IV);
+  return IV;
+}
+
+// Returns a copy of bytes with zeros appended to the end
+// so that the (length of bytes) % 16 == 0.
+function pad16(bytes) {
+  var newBytes = bytes.slice(0);
+  var padding = (16 - (bytes.length % 16)) % 16;
+  for (i = bytes.length; i < bytes.length + padding; i++) {
+    newBytes.push(0);
+  }
+  return newBytes;
+}
+
+// Removes trailing zeros from a byte array.
+function depad(bytes) {
+  var newBytes = bytes.slice(0);
+  while (newBytes[newBytes.length - 1] == 0) {
+    newBytes = newBytes.slice(0, newBytes.length - 1);
+  }
+  return newBytes;
+}
+
+// Wraps a string to 60 characters.
+function wrap60(string) {
+  var outstr = "";
+  for (var i = 0; i < string.length; i++) {
+    if (i % 60 == 0 && i != 0) outstr += "\n";
+    outstr += string[i];
+  }
+  return outstr;
+}
+
+// Returns an MD5 sum of a publicKeyString for easier identification.
+function publicKeyID(publicKeyString) {
+  return MD5(publicKeyString);
+}
+
+function publicKeyFromString(string) {
+  var N = b64to16(string.split("|")[0]);
+  var E = "03";
+  var rsa = new RSAKey();
+  rsa.setPublic(N, E);
+  return rsa;
+}
