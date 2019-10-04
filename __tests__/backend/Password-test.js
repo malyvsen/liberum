50762bcf8a6df00c73f0795e044abb649b8456b6
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-10-04 16:57:10 +0200

    feat(crypto): add password validity checking

--- __tests__/backend/Password-test.js
+++ __tests__/backend/Password-test.js
@@ -1,5 +1,5 @@
 import expect from "expect";
-import { random as randomPassword } from "../../backend/Password";
+import { random as randomPassword, isValid } from "../../backend/Password";
 
 test("same seeds give same passwords", () => {
   const seed = "one two three";
@@ -12,4 +12,15 @@ test("different seeds give different passwords", () => {
   const password = randomPassword("hello there");
   const samePassword = randomPassword("goodbye");
   expect(samePassword).not.toBe(password);
+});
+
+test("randomly generated password is valid", () => {
+  const password = randomPassword("pa$$word");
+  console.log(password);
+  expect(isValid(password)).toBe(true);
+});
+
+test("invalid passwords don't pass isValid", () => {
+  expect(isValid("123456!")).toBe(false);
+  expect(isValid("1234567890")).toBe(false);
 });
--- backend/Password.js
+++ backend/Password.js
@@ -10,5 +10,12 @@ export function random(seed) {
   return result;
 }
 
+export function isValid(password) {
+  return (
+    password.split("").every(letter => alphabet.includes(letter)) &&
+    password.length == length
+  );
+}
+
 export const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 export const length = 7; // 36 ^ 7 = 78,364,164,096 combinations
