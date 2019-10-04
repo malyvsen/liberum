86d884cc339ffd1dfea23a992a43e31502012b77
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-10-04 23:18:31 +0200

    test(crypto): fix misleading arguments passed to Key constructor

--- __tests__/backend/crypto/Key-test.js
+++ __tests__/backend/crypto/Key-test.js
@@ -2,20 +2,20 @@ import expect from "expect";
 import Key from "../../../backend/crypto/Key";
 
 test("same secrets give same public keys", () => {
-  const key = new Key({ secret: "Here we go!", bitLength: 512 });
-  const sameKey = new Key({ secret: "Here we go!", bitLength: 512 });
+  const key = new Key({ secret: "Here we go!" });
+  const sameKey = new Key({ secret: "Here we go!" });
   expect(sameKey.publicKey).toBe(key.publicKey);
 });
 
 test("different secrets give different public keys", () => {
-  const key = new Key({ secret: "Here we go!", bitLength: 512 });
-  const differentKey = new Key({ secret: "No we don't", bitLength: 512 });
+  const key = new Key({ secret: "Here we go!" });
+  const differentKey = new Key({ secret: "No we don't" });
   expect(differentKey.publicKey).not.toBe(key.publicKey);
 });
 
 test("signatures can be verified using public key only", () => {
   const plaintext = "Plaintext to be signed";
-  const signingKey = new Key({ secret: "Here we go!", bitLength: 512 });
+  const signingKey = new Key({ secret: "Here we go!" });
   const verifyingKey = new Key({ publicKey: signingKey.publicKey });
   const signature = signingKey.sign(plaintext);
   const forgedSignature = signature
@@ -27,7 +27,7 @@ test("signatures can be verified using public key only", () => {
 
 test("forged signature is identified as such", () => {
   const plaintext = "Text someone wants to mess with";
-  const signingKey = new Key({ secret: "Moo", bitLength: 512 });
+  const signingKey = new Key({ secret: "Moo" });
   const verifyingKey = new Key({ publicKey: signingKey.publicKey });
   const signature = signingKey.sign(plaintext);
   const forgedSignature = signature
