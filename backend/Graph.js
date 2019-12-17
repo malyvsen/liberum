552aaf84f567298ff6fb16ea5ab9467cf6dcdec5
malyvsen <5940672+malyvsen@users.noreply.github.com>
2019-12-17 22:18:35 +0100

    feat(backend): graph save/load functionality

--- backend/Graph.js
+++ backend/Graph.js
@@ -1,6 +1,13 @@
+import { AsyncStorage } from "react-native";
+import { Account } from "./Account";
+import { Link } from "./Link";
+
+/** The "universe": contains all known accounts, used to log in.
+ * Must be initialized (either loaded from a save or seeded with a new account) before use.
+ */
 export default class Graph {
-  constructor(savePath) {
-    this.accounts = savePath || []; // TODO: load data from save
+  constructor(accounts = []) {
+    this.accounts = accounts;
   }
 
   get loggedIn() {
@@ -27,9 +34,53 @@ export default class Graph {
     console.log("linking with " + accountName); // TODO
   };
 
+  save = async path => {
+    await AsyncStorage.setItem(
+      path,
+      JSON.stringify({
+        saveSystemVersion: saveSystemVersion,
+        accounts: this.accounts,
+        links: this.links
+      })
+    );
+  };
+
+  load = async path => {
+    if (this.accounts != [])
+      throw new Error("cannot load when already not empty");
+    const loaded = await AsyncStorage.getItem(path);
+    const data = JSON.parse(loaded);
+    if (data.saveSystemVersion != saveSystemVersion)
+      throw new Error("mismatched save system version");
+    this.accounts = data.accounts.map(
+      accountData => new Account(accountData.name, accountData.publicKey)
+    );
+    for (let linkData of data.links) {
+      const keys = Object.keys(linkData.signatures);
+      const accounts = keys.map(key =>
+        this.accounts.find(account => account.publicKey == key)
+      );
+      const link = new Link(accounts, linkData.validFrom, linkData.signatures);
+      for (let account of accounts) {
+        account.links.push(link);
+      }
+    }
+  };
+
+  get accounts() {
+    return this.accounts;
+  }
+
+  get links() {
+    const withDuplicates = this.accounts.map(account => account.links).flat();
+    return [...new Set(withDuplicates)]; // this compares references, so linked accounts must share link objects
+  }
+
   get currentAccount() {
     return this.#currentAccount;
   }
 
   #currentAccount = null;
 }
+
+const saveSystemVersion = 0;
--- backend/Link.js
+++ backend/Link.js
@@ -1,7 +1,15 @@
 export default class Link {
-  constructor(accounts, validFrom) {
+  constructor(accounts, validFrom, signatures = {}) {
     this.accounts = accounts;
     this.validFrom = validFrom;
+    this.signatures = signatures;
+    if (
+      Object.keys(this.signatures).some(
+        publicKey =>
+          !this.accounts.map(account => account.publicKey).includes(publicKey)
+      )
+    )
+      throw new Error("all signing accounts must be part of link");
   }
 
   addSignature(account, signature, verify = true) {
@@ -11,13 +19,14 @@ export default class Link {
       if (!account.key.verify(this.toSign, signature))
         throw new Error("invalid signature!");
     }
-    if (this.#signatures[account.key.publicKey])
+    if (this.signatures[account.key.publicKey])
       throw new Error("link already signed by account");
-    this.#signatures[account.key.publicKey] = signature;
+    this.signatures[account.key.publicKey] = signature;
   }
 
+  /** Assumes validity of signatures, only checks if signed by all parties and not expired. */
   get valid() {
-    if (Object.keys(this.#signatures).length !== this.accounts.length)
+    if (Object.keys(this.signatures).length !== this.accounts.length)
       return false;
     return Date.now() < this.validUntil;
   }
@@ -26,7 +35,7 @@ export default class Link {
     return this.validFrom + validityPeriod;
   }
 
-  // string that should be signed by accounts, the same for everyone
+  /** String that should be signed by accounts, the same for everyone. */
   get toSign() {
     const accountNames = this.accounts.map(account => account.name).sort();
     const publicKeys = this.accounts.map(account => account.name).sort();
@@ -34,8 +43,6 @@ export default class Link {
       "&"
     );
   }
-
-  #signatures = {};
 }
 
 const validityPeriod = 30 * 24 * 60 * 60 * 1000;
