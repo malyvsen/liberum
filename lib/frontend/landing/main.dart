17365a7b532ca3a5681f0a9de051b67f455fc49e
Franciszek Madej <f.madej@protonmail.com>
2020-02-29 12:10:27 +0100

    feat(bt): add sample bluetooth code to landing

--- lib/frontend/landing/main.dart
+++ lib/frontend/landing/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class LandingScreen extends StatelessWidget {
   @override
@@ -17,7 +18,19 @@ class LandingScreen extends StatelessWidget {
               padding: const EdgeInsets.all(16),
               onPressed: () {
                 // Navigate to the second screen using a named route.
-                Navigator.pushReplacementNamed(context, '/home');
+                print("app strated");
+                FlutterBlue flutterBlue = FlutterBlue.instance;
+                // Start scanning
+                flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+                // Listen to scan results
+                flutterBlue.scanResults
+                    .listen((scanResult) => print(scanResult));
+
+                // Stop scanning
+                flutterBlue.stopScan();
+
+                Navigator.pushReplacementNamed(context, '/');
               },
             ),
           ]),
