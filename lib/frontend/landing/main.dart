894d82d3d21683586ad2a4d59e8b048f016ea492
Franciszek Madej <f.madej@protonmail.com>
2020-02-29 13:14:23 +0100

    feat(flutter): add state to MyApp <3

--- lib/frontend/landing/main.dart
+++ lib/frontend/landing/main.dart
@@ -1,39 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
+// import 'package:flutter_blue/flutter_blue.dart';
 
 class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: Text("Welcome to Liberum. You've landed!"),
-              padding: const EdgeInsets.all(16),
-            ),
-            RaisedButton(
-              child: Text("Let's go in"),
-              padding: const EdgeInsets.all(16),
-              onPressed: () {
-                // Navigate to the second screen using a named route.
-                print("app strated");
-                FlutterBlue flutterBlue = FlutterBlue.instance;
-                // Start scanning
-                flutterBlue.startScan(timeout: Duration(seconds: 4));
-
-                // Listen to scan results
-                flutterBlue.scanResults
-                    .listen((scanResult) => print(scanResult));
-
-                // Stop scanning
-                flutterBlue.stopScan();
-
-                Navigator.pushReplacementNamed(context, '/');
-              },
-            ),
-          ]),
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(
+          child: Text("Welcome to Liberum. You've landed!"),
+          padding: const EdgeInsets.all(16),
+        ),
+        RaisedButton(
+          child: Text("Let's go in"),
+          padding: const EdgeInsets.all(16),
+          onPressed: () {
+            // fix this somehow
+            Navigator.pushReplacementNamed(context, '/');
+            Navigator.of(context).popUntil((route) => route.isFirst);
+          },
+        ),
+      ])),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:liberum/frontend/home/main.dart';
 import 'package:liberum/frontend/landing/main.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Liberum',
-    // Start the app with the "/" named route. In this case, the app starts
-    // on the FirstScreen widget.
-    initialRoute: '/landing',
-    routes: {
-      // When navigating to the "/" route, build the FirstScreen widget.
-      '/landing': (context) => LandingScreen(),
-      // When navigating to the "/second" route, build the SecondScreen widget.
-      '/': (context) => HomeScreen(),
-    },
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var account = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Liberum',
+      // Start the app with the "/" named route. In this case, the app starts
+      // on the FirstScreen widget.
+      initialRoute: '/landing',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/landing': (context) => LandingScreen(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/': (context) => HomeScreen(),
+      },
+    );
+  }
 }
